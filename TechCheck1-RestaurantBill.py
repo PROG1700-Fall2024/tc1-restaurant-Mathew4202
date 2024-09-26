73b214ded2c1b47c6b4a09cf088c21f339fd15e7
Matew4202 <w0461513@campus.nscc.ca>
2024-09-26 11:36:19 -0300

    Techcheck 1 done

--- TechCheck1-RestaurantBill.py
+++ TechCheck1-RestaurantBill.py
@@ -7,10 +7,38 @@
 # The program will then output the amount of the tax (15% of the original amount) and a tip (20% of the original amount). 
 # Finally, the program will output the new total of the bill.
 
-
+"""
+Student Name:  Mathew Akunyili  
+Program Title: Tech check 1
+Description:    Creat a program that calculates the total bil in a resturant 
+"""
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    # welcome user to program
+    print("Hello welcome to our program, we will help you calculate the tax and tip of you bill")
+
+    #prompt user to input bill
+    bill = input("Please enter your original bill amount:")
+
+    #Calculate tax of bill
+    tax = int(bill) * 0.15
+
+    #calculate tip of bill
+    tip = int(bill) * 0.20
+
+    #Print original bill to user
+    print("Your original bill amount is: $"+bill)
+
+    #Print tax to user
+    print("Your tax is:", tax)
+
+    #print tip to user
+    print("Your tip is:", float(tip))
+
+    #print total bil to user
+    total = float(bill) + float(tip) + float(tax)
+    print("Your total is: $"+str(total))
 
 
 
